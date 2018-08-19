8816a9883c08f6968d44bfe670fb6585477ec10d
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-19 21:58:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,18 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,num)
+  if board[num]==" "||board[num]==""||board[num]==nil
+   false
+  else
+   true
+  end
+end
 def valid_move(num)
-  if num.between?(1,9)
+  if num.between?(1,9)&& !position_taken?
+    true
+  else
+    false
+  end
+end
     
