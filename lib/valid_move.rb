e5475c8a9d01d9b2e9e7bd3823929d5954f5a237
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-19 22:07:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,4 @@
 # code your #valid_move? method here
-
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,num)
   if board[num]==" "||board[num]==""||board[num]==nil
@@ -11,7 +9,7 @@ def position_taken?(board,num)
 end
 def valid_move?(board,num)
   num=num.to_i
-  if num.between?(1,9)&& !position_taken?(board,num)==true
+  if num.between?(1,9)&& !position_taken?(board,num)
     true
   else
     false
